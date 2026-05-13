333474e6da8a87f0d474c9211fd210bda2dbbaaf
Blake Printy <blakehprinty@gmail.com>
2026-05-13 00:37:03 -0400

    Initial Commit

---
+++ src/main/java/com/blakeprinty/ApexLegendsAPI/ApexLegendsApiApplication.java
@@ -0,0 +1,13 @@
+package com.blakeprinty.ApexLegendsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexLegendsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexLegendsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blakeprinty/ApexLegendsAPI/ApexLegendsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blakeprinty.ApexLegendsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexLegendsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
